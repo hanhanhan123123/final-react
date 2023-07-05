5843a058e953348ea39869c30adc44fd12e6dda4
sj <suta0070@naver.com>
2023-07-05 15:56:06 +0900

    react commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RecoilRoot, atom } from 'recoil';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import KakaoCallBackPage from './pages/KakaoCallBackPage';
+import HomePage from './pages/HomePage';
+import DeactivatePage from './pages/settings/DeactivatePage';
+import DeactivatedPage from './pages/settings/DeactivatedPage';
+import ProfilePage from './pages/settings/ProfilePage';
+
+
+//==========================================================
+export const jwtState = atom(
+  { key: "jwtState", default: null }
+);
+export const userEmailState = atom(
+  { key: "userEmailState", default: null }
+);
+//============================================================
+
+const router = createBrowserRouter([
+  { path: "/", element: <IndexPage />, },
+  { path: "/flow/login", element: <LoginPage /> },
+  { path: "/flow/signup", element: <SignupPage /> },
+  { path: "/flow/kakao/callback", element: <KakaoCallBackPage /> },
+  { path: "/home", element: <HomePage /> },
+  { path: "/settings/profile", element: <ProfilePage /> },
+  { path: "/settings/deactivate", element: <DeactivatePage /> },
+  { path: "/settings/deactivated", element: <DeactivatedPage /> }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <RouterProvider router={router} />
+  </RecoilRoot>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
